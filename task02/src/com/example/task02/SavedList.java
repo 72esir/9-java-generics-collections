d164fa02f5daa95742d394e192099422e8ca3f84
Aleksey Salamatin <asalamatin640@gmail.com>
2024-11-13 22:07:37 +0500

    Salamatin Aleksey Task02

--- task02/src/com/example/task02/SavedList.java
+++ task02/src/com/example/task02/SavedList.java
@@ -1,35 +1,66 @@
 package com.example.task02;
 
-import java.io.File;
-import java.io.Serializable;
+import javafx.beans.binding.When;
+
+import java.io.*;
 import java.util.AbstractList;
+import java.util.ArrayList;
+import java.util.List;
 
 public class SavedList<E extends Serializable> extends AbstractList<E> {
 
-    public SavedList(File file) {
+    private List<E> list;
+    private File file;
+
+    public SavedList(File file) throws IOException {
+        if (file.exists()){
+            try(ObjectInputStream objectInputStream = new ObjectInputStream(new FileInputStream(file))){
+                this.list = (List<E>) objectInputStream.readObject();
+            }catch (ClassNotFoundException e){
+                e.printStackTrace();
+            }
+        }
+        else{
+            this.list = new ArrayList<>();
+        }
+        this.file = file;
     }
 
     @Override
     public E get(int index) {
-        return null;
+        return list.get(index);
     }
 
     @Override
     public E set(int index, E element) {
-        return null;
+        E val = list.set(index,element);
+        changedList();
+        return val;
     }
 
     @Override
     public int size() {
-        return 0;
+        return list.size();
     }
 
     @Override
     public void add(int index, E element) {
+        list.add(index,element);
+        changedList();
     }
 
     @Override
     public E remove(int index) {
-        return null;
+        E val = list.remove(index);
+        changedList();
+        return val;
+    }
+
+    public void changedList() {
+        try(ObjectOutputStream objectOutputStream = new ObjectOutputStream(new FileOutputStream(file))){
+            objectOutputStream.writeObject(list);
+        }catch (IOException e){
+            e.printStackTrace();
+        }
     }
 }
